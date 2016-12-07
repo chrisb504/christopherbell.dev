6a1b116b0bf9e04de425bc9dcb89528ecf2998f8
Christopher Bell <chrisotpherbell1813@gmail.com>
2016-12-06 23:44:40 -0600

    starting functions for game.

--- js/azurras.js
+++ js/azurras.js
@@ -3,17 +3,45 @@ function Main() {
 }
 
 function SurivialGame() {
-	function Player(playerName, playerHealth) {
-		this.playerName = playerName;
+
+	function Player(playerHealth, playerLevel playerName, playerStamina, playerStength) {
 		this.playerHealth = playerHealth;
+		this.playerLevel = playerLevel
+		this.playerName = playerName;
+
+		
+
+
+		this.gainHealth = function(amountToGain) {
+			this.playerHealth += amountToGain;
+		}
+
+		this.getPlayerHealth = function() {
+			return this.playerHealth;
+		}
+
+		this.getPlayerName = function() {
+			return this.playerName;
+		}
+
 		this.reduceHealth = function(amountToReduce) {
 			this.playerHealth -= amountToReduce;
 		}
+
+		this.setPlayerHealth = function(newHealth) {
+			this.playerHealth = newHealth;
+		}
+
+		this.setPlayerName = function(newName) {
+			this.playerName = newName;
+		}
 	}
+
 
 	function Inventory() {
 
 	}
+
 	
 	function Enemy(enemyName, enemyHealth) {
 		this.enemyName = enemyName;
@@ -21,8 +49,8 @@ function SurivialGame() {
 	}
 
 	// Test stuff
-	player = new Player("Chris", 10);
-	player.reduceHealth(5);
-	console.log(player.playerHealth);
+	//player = new Player("Chris", 10);
+	//player.reduceHealth(5);
+	//console.log(player.playerHealth);
 }
 
