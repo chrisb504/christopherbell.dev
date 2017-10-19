15cb5b9dcbd88cdfa4e5c2d0586b01a1735219ff
Christopher Bell <christopherbell1813@gmail.com>
2017-10-18 22:52:13 -0500

    Formatting changes to some files

--- apps/lunch-gen/js/restaurant.js
+++ apps/lunch-gen/js/restaurant.js
@@ -1,21 +1,21 @@
 (function () {
     var restaurant = angular.module('restaurant', []);
-    restaurant.directive("restaurantDirective", function() {
+    restaurant.directive("restaurantDirective", function () {
         return {
-            templateUrl : 'apps/lunch-gen/html/restaurant.html',
+            templateUrl: 'apps/lunch-gen/html/restaurant.html',
             scope: {}
         };
     });
-    
-    restaurant.controller('restaurantController', function($scope, $http, RestaurantFactory, $window) {
+
+    restaurant.controller('restaurantController', function ($scope, $http, RestaurantFactory, $window) {
         vm = this;
         $scope.articles;
-        
+
         RestaurantFactory.get().success(function (msg) {
             $scope.articles = msg.Articles;
         });
     });
-    
+
     restaurant.factory('RestaurantFactory', function ($http) {
         return {
             get: function () {
