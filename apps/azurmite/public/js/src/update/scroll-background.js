8474e360eb5101de107f3774e9783021ca5e1014
Christopher Bell <christopherbell1813@gmail.com>
2018-11-18 20:38:26 -0600

    removed broken code

--- apps/azurmite/public/js/src/update/scroll-background.js
+++ apps/azurmite/public/js/src/update/scroll-background.js
@@ -1,4 +1,4 @@
         demoBackgroundImage.tilePosition.x -= 1;
 
-        game.physics.overlap(player, funkFlower, player.Kill(), null);
-        game.physics.overlap(player, funkFlower);
+        //game.physics.overlap(player, funkFlower, player.Kill(), null);
+        //game.physics.overlap(player, funkFlower);
