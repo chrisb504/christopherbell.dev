3e622638edb9e4b335e24f07c72905051b352458
Christopher Bell <Christopherbell1813@gmail.com>
2019-01-19 21:03:14 -0600

    Moved to new location

---
+++ public/js/whats-for-lunch/maincontroller.mjs
@@ -0,0 +1,15 @@
+import * as restaurant from './restaurantcontroller.mjs';
+
+$(document).ready(function () {
+    if($('#button-add').length > 0) {
+        $('#button-add').on('click', restaurant.addRestaurant);
+    }
+    
+    if($('#button-delete').length > 0) {
+        $('#button-delete').on('click', restaurant.deleteRestaurantWithID);
+    }
+
+    if($('#getRestaurantData').length > 0) {
+        restaurant.getData(restaurant.populateRestaurantTable);
+    }
+});
