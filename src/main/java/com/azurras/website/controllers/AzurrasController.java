4466a035c95351c7467f65c1a666f217cfe765cb
Christopher Bell <cbell7@icloud.com>
2020-01-28 21:32:54 -0600

    Added needed controllers and resource files

--- src/main/java/com/azurras/website/controllers/AzurrasController.java
+++ src/main/java/com/azurras/website/controllers/AzurrasController.java
@@ -1,13 +1,27 @@
 package com.azurras.website.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class AzurrasController {
-    @RequestMapping("/")
+
+    // @RequestMapping("/")
+    // @ResponseBody
+    // public String index() {
+    //     return "index";
+    // }
+
+    @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "index";
+    }
+
+    @GetMapping("/resume")
+    public String resume() {
+        return "resume";
     }
 }
---
+++ src/main/java/com/azurras/website/controllers/WhatsForLunchController.java
@@ -0,0 +1,5 @@
+package com.azurras.website.controllers;
+
+public class WhatsForLunchController {
+
+}
