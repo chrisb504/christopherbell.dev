e234a21230de8cb85cdec59b62ae8782d88a463e
Christopher Bell <cbell7@icloud.com>
2020-02-02 21:00:46 -0600

    Removed some unneed items from the controller

--- src/main/java/com/azurras/website/controllers/AzurrasController.java
+++ src/main/java/com/azurras/website/controllers/AzurrasController.java
@@ -1,19 +1,10 @@
 package com.azurras.website.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AzurrasController {
-
-    // @RequestMapping("/")
-    // @ResponseBody
-    // public String index() {
-    //     return "index";
-    // }
 
     @GetMapping("/")
     public String index() {
