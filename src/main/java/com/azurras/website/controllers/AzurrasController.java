4c1b38f6c94faad5a14250de8c90d4749915f4da
Christopher Bell <cbell7@icloud.com>
2020-05-31 13:57:55 -0500

    Updated codebase for flush podcast

--- src/main/java/com/azurras/website/controllers/AzurrasController.java
+++ src/main/java/com/azurras/website/controllers/AzurrasController.java
@@ -3,6 +3,11 @@ package com.azurras.website.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * Purpose of this controller is to return top level secondary pages
+ * for the website. This includes pages like blog. If the blog page had any childern
+ * pages than the blog controller should handle outputing those pages.
+ */
 @Controller
 public class AzurrasController {
 
@@ -11,8 +16,28 @@ public class AzurrasController {
         return "index";
     }
 
+    @GetMapping("/azurmite")
+    public String azurmite() {
+        return "azurmite";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+
+    @GetMapping("/flush")
+    public String flush() {
+        return "flush";
+    }
+
     @GetMapping("/resume")
     public String resume() {
         return "resume";
+    }
+
+    @GetMapping("/whatsforlunch")
+    public String whatsforlunch() {
+        return "whatsforlunch";
     }
 }
---
+++ src/main/java/com/azurras/website/services/AzurrasService.java
@@ -0,0 +1,5 @@
+public class AzurrasService {
+    public AzurrasService() {
+
+    }
+}
