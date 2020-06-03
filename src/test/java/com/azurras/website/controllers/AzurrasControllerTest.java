1d463e308b3b63d988a0c4f4e73abd412643711a
Christopher Bell <cbell7@icloud.com>
2020-06-03 11:38:57 -0500

    Adding starting java test classes

---
+++ src/test/java/com/azurras/website/controllers/AzurrasControllerTest.java
@@ -0,0 +1,17 @@
+package com.azurras.website.controllers;
+
+import com.azurras.website.configs.GeneralConfig;
+
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@EnableConfigurationProperties
+@ContextConfiguration(classes={GeneralConfig.class}, initializers = ConfigFileApplicationContextInitializer.class)
+public class AzurrasControllerTest {
+    
+}
