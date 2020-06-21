8bdeffdef48e338e5d18ffb59ae41be2d17a176a
Christopher Bell <cbell7@icloud.com>
2020-06-21 14:52:38 -0500

    New error constant

--- src/main/java/com/azurras/website/utils/Constants.java
+++ src/main/java/com/azurras/website/utils/Constants.java
@@ -8,4 +8,6 @@ import org.apache.logging.log4j.Logger;
 public final class Constants {
     private final Log LOG = LogFactory.getLog(Constants.class);
 
+    public final static String BLOG_NULL_REQUEST = "ERROR: A Null Request was received.";
+
 }
