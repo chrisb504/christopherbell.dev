287ab5d721fe8c93ec3db813382bff8e05e6bc08
Christopher Bell <cbell7@icloud.com>
2020-06-21 15:43:53 -0500

    Added java docs to AzurrasController.java

--- src/main/java/com/azurras/website/controllers/AzurrasController.java
+++ src/main/java/com/azurras/website/controllers/AzurrasController.java
@@ -9,11 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Purpose of this controller is to return top level secondary pages for the
- * website. This includes pages like blog. If the blog page had any childern
- * pages than the blog controller should handle outputing those pages.
- */
 @Controller
 public class AzurrasController {
     private final Log LOG = LogFactory.getLog(AzurrasController.class);
@@ -24,12 +19,21 @@ public class AzurrasController {
         this.azurrasService = azurrasService;
     }
 
+    /**
+     * Returns the home page.
+     * 
+     * @return index
+     */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getHomepage() {
-        LOG.info("test");
         return "index";
     }
 
+    /**
+     * Returns the resume page.
+     * 
+     * @return resume
+     */
     @RequestMapping(value = "/resume", method = RequestMethod.GET)
     public String getResume() {
         return "resume";
