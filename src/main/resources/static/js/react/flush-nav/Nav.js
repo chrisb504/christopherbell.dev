552043270d7c1abfe7f589eefe5c9afe0cc01fbe
Christopher Bell <cbell7@icloud.com>
2020-06-30 18:35:26 -0500

    Updates to include new blog post

--- src/main/resources/static/js/react/flush-nav/Nav.js
+++ src/main/resources/static/js/react/flush-nav/Nav.js
@@ -4,7 +4,7 @@ export default class Nav extends Component {
     render() {
         return (
             <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                <a class="navbar-brand" href="/">Flush Media</a>
+                <a class="navbar-brand" href="/flush">Flush Media</a>
                 <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
