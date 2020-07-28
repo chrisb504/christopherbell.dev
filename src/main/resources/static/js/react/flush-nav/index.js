730616f72b0b2c1818d13483dc2e37a4f25314e5
Christopher Bell <cbell7@icloud.com>
2020-07-28 11:36:30 -0500

    Removed unneeded files and fixed a couple of issues.

--- src/main/resources/static/js/react/flush-nav/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Nav from './Nav';
-
-window.addEventListener('load', () => {
-    if (document.body.contains(document.getElementById('flush-nav'))) {
-        ReactDOM.render(<Nav />, document.getElementById('flush-nav'));
-    }
-});
--- src/main/resources/static/js/react/nav/Nav.js
+++ src/main/resources/static/js/react/nav/Nav.js
@@ -4,7 +4,7 @@ export default class Nav extends Component {
     render() {
         return (
             <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" role="navigation">
-                <a class="navbar-brand" href="/">Azurras</a>
+                <a class="navbar-brand" href="/">Home</a>
                 <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -14,13 +14,7 @@ export default class Nav extends Component {
                             <a class="nav-link" href="/blog">Blog</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/resume">Resume</a>
-                        </li>
-                        <li class="nav-item">
                             <a class="nav-link" href="/smp">SMP</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/wfl">What's For Lunch</a>
                         </li>
                     </ul>
                 </div>
