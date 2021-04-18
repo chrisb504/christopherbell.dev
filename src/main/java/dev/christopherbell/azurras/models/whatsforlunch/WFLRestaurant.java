b6600a437673e838f70c87439cf5cc6aabc49e81
Christopher Bell <cbell7@icloud.com>
2021-04-18 10:56:07 -0500

    made this a managed class

--- src/main/java/dev/christopherbell/azurras/models/whatsforlunch/WFLRestaurant.java
+++ src/main/java/dev/christopherbell/azurras/models/whatsforlunch/WFLRestaurant.java
@@ -1,8 +1,14 @@
 package dev.christopherbell.azurras.models.whatsforlunch;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "wfl_table")
 public class WFLRestaurant {
     private String address;
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
     private String name;
     private String phoneNumber;
     private String postalCode;
