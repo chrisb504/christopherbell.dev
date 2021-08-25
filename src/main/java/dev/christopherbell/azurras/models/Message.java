72799350afa5231d145c47e68edf02087cdf9935
Azurras <cbell7@icloud.com>
2021-08-24 22:31:25 -0500

    Updating Message class

--- src/main/java/dev/christopherbell/azurras/models/Message.java
+++ src/main/java/dev/christopherbell/azurras/models/Message.java
@@ -2,17 +2,12 @@ package dev.christopherbell.azurras.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 @Component
 public class Message {
-    @Autowired
     private String description;
-    @Autowired
     private String key;
 }
