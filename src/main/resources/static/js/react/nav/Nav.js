b8fe6b734f9b747858097712b2045ecce7318115
Azurras <cbell7@icloud.com>
2021-08-26 19:11:02 -0500

    Fixed issue where nav was not centered right

--- src/main/resources/static/js/react/nav/Nav.js
+++ src/main/resources/static/js/react/nav/Nav.js
@@ -3,29 +3,31 @@ import React, { Component } from 'react';
 export default class Nav extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" role="navigation">
-                <a class="navbar-brand" href="/">Home</a>
-                <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="navbar-collapse collapse">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/blog">Blog</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Photography
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                                <li><a class="dropdown-item" href="/photography">Gallery</a></li>
-                                <li><a class="dropdown-item" href="/photography/usage">Usage Notice</a></li>
-                            </ul>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="/">Home</a>
+                    <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="navbar-collapse collapse">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="/blog">Blog</a>
                             </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/smp">SMP</a>
-                        </li>
-                    </ul>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Photography
+                                </a>
+                                <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="/photography">Gallery</a></li>
+                                    <li><a class="dropdown-item" href="/photography/usage">Usage Notice</a></li>
+                                </ul>
+                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/smp">SMP</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         );
