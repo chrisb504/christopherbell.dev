91b7b6ce0ba58f05e6540088ba10a5eabd328299
Christopher Bell <cbell7@icloud.com>
2022-02-22 22:02:59 -0600

    Updated survive game to be a react component.

--- src/main/resources/static/js/components/survive/main.mjs
+++
@@ -1,8 +0,0 @@
-import Player from './models/Entity/Player.mjs'
-import Constants from './util/Constants.mjs'
-
-(function() {
-    let player = new Player("Player");
-    console.log(player);
-    
-})();
