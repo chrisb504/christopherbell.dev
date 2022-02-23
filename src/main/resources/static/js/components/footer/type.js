cb192c86cba77367a8bf08ef29a4f14ec2cc2c93
Christopher Bell <cbell7@icloud.com>
2022-02-22 22:12:35 -0600

    Changing name of footer

--- src/main/resources/static/js/components/footer/type.js
+++ src/main/resources/static/js/components/footer/type.js
@@ -1,5 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+/**
+ * This component represents the footer on each page.
+ */
 export default class Footer extends Component {
     render() {
         return (
@@ -24,3 +28,19 @@ export default class Footer extends Component {
         );
     } 
 }
+
+/* 
+    This seems to be important from my findings. If the page doesn't include that component,
+    then it will still try to render without the EventListener. This will cause React to throw
+    an error about not finding the DOM item. This method prevents the render function from
+    loading if the component isn't on the page.
+
+    I think there might be a better solution to this. I think that solution might include
+    not including the js for smaller components in Main.js
+*/
+window.addEventListener('load', () => {
+    if (document.body.contains(document.getElementById('footer'))) {
+        // Used to render the component on the page. The default refresh is when state is changed.
+        ReactDOM.render(<Footer />, document.getElementById('footer'));
+    }
+});
