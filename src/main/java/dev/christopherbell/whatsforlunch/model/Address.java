6e246cc49135bd4a7cfb9255b9689dbf5ef3c77a
Chris <cbell7@icloud.com>
2024-11-10 21:55:53 -0600

    - clean up json props (#976)
    * - clean up json props
    
    * - added thebell code
    
    * - added thebell code

--- src/main/java/dev/christopherbell/whatsforlunch/model/Address.java
+++ src/main/java/dev/christopherbell/whatsforlunch/model/Address.java
@@ -8,7 +8,9 @@ import lombok.Data;
 public class Address {
 
   private String city;
-  private String state;
-  private String streetName;
+  private String county;
   private String postalCode;
+  private String state;
+  private String street1;
+  private String street2;
 }
