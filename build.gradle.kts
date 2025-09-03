9f28c1522f51685a830b436e82b1fcd2b96a0832
Christopher Bell <cbell7@icloud.com>
2025-09-02 22:46:29 -0500

    Removed the Void package and all related code. Remove JPA dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.8")
     implementation("org.springframework.boot:spring-boot-starter-json")
--- src/main/java/dev/christopherbell/thevoid/invite/InviteCodeRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.christopherbell.thevoid.invite;
-
-import dev.christopherbell.thevoid.invite.model.InviteCodeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InviteCodeRepository extends JpaRepository<InviteCodeEntity, Long> {
-
-}
