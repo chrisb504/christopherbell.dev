0589428fd0716791e11ffbc2fd12dfc764b9b66a
Christopher Bell <cbell7@icloud.com>
2025-09-02 22:52:10 -0500

    Removed the Void package and all related code. Remove JPA dependency. (#1055)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
-rootProject.name = "cbell-website"
+rootProject.name = "website"
 
 
--- src/main/java/dev/christopherbell/thevoid/account/VoidAccountDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.christopherbell.thevoid.account;
-
-import dev.christopherbell.thevoid.account.model.entity.AccountDetailsEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoidAccountDetailsRepository extends JpaRepository<AccountDetailsEntity, Long> {
-
-}
