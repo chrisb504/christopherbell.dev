f9cdbf479f4a5e2625ac3f0bed442e0038b9bbf5
Christopher Bell <cbell7@icloud.com>
2025-09-14 17:03:42 -0500

    Make these proper utility classes

--- cbell-lib/src/main/java/dev/christopherbell/libs/api/APIConstants.java
+++ cbell-lib/src/main/java/dev/christopherbell/libs/api/APIConstants.java
@@ -1,17 +1,15 @@
 package dev.christopherbell.libs.api;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * Common API constants used across services and libraries.
  */
+@UtilityClass
 public final class APIConstants {
-
-  private APIConstants() {
-  }
-
   public static final String EMPTY_STRING = "";
   public static final String FAILURE = "failure";
   public static final String SUCCESS = "success";
-
   public static final String VALIDATION_BAD_CLIENT_ID = "The client's ID is not valid.";
   public static final String VALIDATION_BAD_EMAIL = "The given email is not valid.";
   public static final String VALIDATION_BAD_PASSWORD = "The given password is not valid.";
--- cbell-lib/src/main/java/dev/christopherbell/libs/api/APIVersion.java
+++ cbell-lib/src/main/java/dev/christopherbell/libs/api/APIVersion.java
@@ -1,11 +1,14 @@
 package dev.christopherbell.libs.api;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * Path fragments for API versioning.
  *
  * <p>Use these constants when defining versioned route mappings.</p>
  */
-public class APIVersion {
+@UtilityClass
+public final class APIVersion {
   public static final String V20241215 = "/2024-12-15";
   public static final String V20250903 = "/2025-09-03";
   public static final String V20250912 = "/2025-09-12";
