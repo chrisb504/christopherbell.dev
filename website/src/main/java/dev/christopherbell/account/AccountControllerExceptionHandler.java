3e151f0ba70349cfa7ed904ff192d65eca5e0733
Christopher Bell <cbell7@icloud.com>
2025-09-14 15:12:36 -0500

    Addressed PR Comments

--- website/src/main/java/dev/christopherbell/account/AccountControllerExceptionHandler.java
+++ website/src/main/java/dev/christopherbell/account/AccountControllerExceptionHandler.java
@@ -4,10 +4,13 @@ import dev.christopherbell.libs.api.model.Message;
 import dev.christopherbell.libs.api.model.Response;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * Exception handler for AccountController to handle specific account-related exceptions
@@ -15,8 +18,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
  */
 @ControllerAdvice
 @Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class AccountControllerExceptionHandler {
-  private final static String ACCOUNT_NOT_ACTIVE_CODE = "005";
+  private final static String ACCOUNT_NOT_ACTIVE_CODE = "ACCOUNT_NOT_ACTIVE";
 
   /**
    * Handles AccountNotActiveException and returns a 400 Bad Request response with an error message.
@@ -25,15 +29,15 @@ public class AccountControllerExceptionHandler {
    * @return ResponseEntity with error details
    */
   @ExceptionHandler({AccountNotActiveException.class})
-  public ResponseEntity<Response<?>> handleAccountNotActiveException(AccountNotActiveException e) {
-    log.error(e.getMessage(), e);
-    return new ResponseEntity<>(
-        Response.builder()
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public Response<?> handleAccountNotActiveException(AccountNotActiveException e) {
+    log.error(ACCOUNT_NOT_ACTIVE_CODE, e);
+    return Response.builder()
             .messages(List.of(Message.builder()
                 .code(ACCOUNT_NOT_ACTIVE_CODE)
                 .description(e.getMessage())
                 .build()))
             .success(false)
-            .build(), HttpStatus.BAD_REQUEST);
+            .build();
   }
 }
